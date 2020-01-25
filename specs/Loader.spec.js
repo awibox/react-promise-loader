8495dbe42a0097f02dd3d0eebd24fec26aff6042
Andrei Arkhipov <andrei@awb.pw>
2020-01-25 14:35:54 +0300

    #3: Problem with promiseTracker was fixed

--- specs/Loader.spec.js
+++ specs/Loader.spec.js
@@ -7,4 +7,12 @@ describe('Loader', () => {
     const component = shallow(<Loader loading={true} />);
     expect(component).toMatchSnapshot();
   });
+  it('should not render', () => {
+    const component = shallow(<Loader />);
+    expect(component.get(0)).toEqual(null);
+  });
+  it('should render for promiseTracker', () => {
+    const component = shallow(<Loader promiseTracker={() => ({ promiseInProgress: true })} />);
+    expect(component).toExist();
+  });
 });
